877aa7714932672b3c783d7d4da851b67bcf567e
Tanguy <tanguy.francois@epfedu.fr>
2023-09-25 19:25:14 +0200

    exo 1 tanguy françois

---
+++ premierProjetSurGithub/Exo1/src/main/java/com/mycompany/exo1/Exo1.java
@@ -0,0 +1,29 @@
+/*
+ * Tanguy Francois TDC
+ * TPO EXO1
+ */
+
+package com.mycompany.exo1;
+
+/**
+ *
+ * @author Tanguy
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        
+        System.out.println("Bonjour");
+        System.out.println("aurevoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in); 
+        System.out.println("Comment vous appelez vous ?"); 
+        prenom = sc.nextLine();
+    }
+    
+}
+
