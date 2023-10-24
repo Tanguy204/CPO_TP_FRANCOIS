792cab88084f0ee470f3979c1dcf1af02b1259f8
Tanguy <tanguy.francois@epfedu.fr>
2023-10-24 13:02:45 +0200

    en cours

---
+++ LightOff_FRANCOIS_version_console/src/lightoff_francois_version_console/LightOff_FRANCOIS_version_console.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_francois_version_console;
+
+/**
+ *
+ * @author Tanguy
+ */
+public class LightOff_FRANCOIS_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GrilleDeJeu grille = new GrilleDeJeu(5,5);
+        System.out.println(grille);
+    }
+    
+}
