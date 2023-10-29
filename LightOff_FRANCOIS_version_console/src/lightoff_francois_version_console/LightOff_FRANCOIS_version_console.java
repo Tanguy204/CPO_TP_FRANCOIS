11005b44b84f3716b9303fe80bfe7b91bb18ff2f
Tanguy <tanguy.francois@epfedu.fr>
2023-10-29 17:09:11 +0100

    amélioration niveau terminée

--- LightOff_FRANCOIS_version_console/src/lightoff_francois_version_console/LightOff_FRANCOIS_version_console.java
+++ LightOff_FRANCOIS_version_console/src/lightoff_francois_version_console/LightOff_FRANCOIS_version_console.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lightoff_francois_version_console;
+import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  *
@@ -14,8 +16,27 @@ public class LightOff_FRANCOIS_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GrilleDeJeu grille = new GrilleDeJeu(5,5);
-        System.out.println(grille);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("bienvenue sur LightOff");
+        System.out.println("Taper 1 pour jouer en mode libre ou taper 2 pour les niveaux");
+        int LoN = scanner.nextInt();
+        if (LoN==2){
+            ModeNiveaux niveau = new ModeNiveaux();
+            niveau.LancerNiveaux();
+        }
+        if (LoN==1  ){
+            System.out.println("indiquer le nombre de ligne que vous voulez sur votre grille");
+            int x = scanner.nextInt();
+            System.out.println("indiquer le nombre de colonne que vous voulez sur votre grille");
+            int y = scanner.nextInt();
+            System.out.println("indiquer le nombre de coups max pour réaliser la grille");
+            int nbC = scanner.nextInt();
+            Partie partie1= new Partie(x,y,nbC);
+            partie1.initialiserPartie();
+            partie1.lancerPartie();
+        }  
+        
+                
     }
     
 }
